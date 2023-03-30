2a442d3e14e0336e8bf85016e13573393a47c75b
Chris Jacob <chrisjacob876@gmail.com>
2023-03-30 16:04:23 +0530

    feat:tast-completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,69 @@
 import "./App.css";
 import { useEffect } from "react";
 import { fabric } from "fabric";
-
+import "./PenAndEraser.css";
 function App() {
+  let canvas;
   useEffect(() => {
-    const canvas = new fabric.Canvas("canvas");
+    canvas = new fabric.Canvas("canvas");
     canvas.setHeight(window.innerHeight);
     canvas.setWidth(window.innerWidth);
     canvas.isDrawingMode = true;
     canvas.freeDrawingBrush.width = 3;
-    canvas.freeDrawingBrush.color = "#0052cc";
+    canvas.freeDrawingBrush.color = "red";
   }, []);
-
-  return <canvas id="canvas" />;
+  function changeSize(e) {
+    canvas.freeDrawingBrush.width = e.target.value;
+  }
+  function changeColor(color) {
+    console.log(color);
+    canvas.freeDrawingBrush.color = color;
+  }
+  function eraser() {
+    canvas.freeDrawingBrush.color = "#ffffff";
+  }
+  return (
+    <>
+      <div style={{ display: "flex" }}>
+        <input
+          id="brush-size"
+          type="range"
+          min="1"
+          max="20"
+          onChange={(e) => changeSize(e)}
+        />
+        <div
+          className="round"
+          onClick={() => changeColor("black")}
+          style={{ backgroundColor: "black" }}
+        ></div>
+        <div
+          className="round"
+          onClick={() => changeColor("green")}
+          style={{ backgroundColor: "green" }}
+        ></div>
+        <div
+          className="round"
+          onClick={() => changeColor("yellow")}
+          style={{ backgroundColor: "yellow" }}
+        ></div>
+        <div
+          className="round"
+          onClick={() => changeColor("blue")}
+          style={{ backgroundColor: "blue" }}
+        ></div>
+        <div
+          className="round"
+          onClick={() => changeColor("red")}
+          style={{ backgroundColor: "red" }}
+        ></div>
+        <button onClick={eraser} style={{ backgroundColor: "lightblue" }}>
+          Eraser
+        </button>
+      </div>
+      <canvas id="canvas" />
+    </>
+  );
 }
 
 export default App;
